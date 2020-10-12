d6819a7b5fb5f9ef254fefe221f569781485a8fc
John Ansell <jfansell@gmail.com>
2020-10-12 18:31:32 +0200

    Work on project up and to 12/10/2020

--- Proj_2_script.R
+++ Proj_2_script.R
@@ -19,3 +19,44 @@ download.file(url_white_wine, "datasets/white_wine.csv")
 rm(url_red_wine)
 rm(url_white_wine)
 
+##Import the CSV files into R - delimited with ;
+red_wine <- read_delim("datasets/red_wine.csv", ";")
+white_wine <- read_delim("datasets/white_wine.csv", ";")
+
+##Verify columns are identical between white and red wine
+identical(colnames(red_wine), colnames(white_wine))
+
+##List predictor variables for description in the report
+colnames(red_wine)[1:11]
+
+##Merge wine sets into a single set
+red_temp <- red_wine %>%
+  mutate(type = "Red")
+
+white_temp <- white_wine %>%
+  mutate(type = "White")
+
+wine_combined <- bind_rows(white_temp, red_temp)
+
+##Tidy up intermediate step variables
+rm(white_temp, red_temp)
+
+##wine type as factor
+wine_combined$type <- as.factor(wine_combined$type)
+
+##Plot the distribution of quality as overlaid density function (adjust = 2 to smooth)
+wine_combined %>%
+  ggplot(aes(quality, fill = type)) +
+  geom_density(alpha = 0.4, adjust = 2)
+
+##Boxplots!
+wine_combined %>%
+  ggplot(aes(type, quality)) +
+  geom_boxplot(aes(colour = type)) +
+  xlab("Wine type")
+
+##Summary statistics of ratings by wine type
+wine_combined %>%
+  group_by(type) %>%
+  summarise(mean = mean(quality), std_dev = sd(quality), median = median(quality)) %>%
+  knitr::kable()
